519f40557f7c995a1ec63050488faf2dfc626961
DESKTOP-5V3FRMH\M <marhalroutemarhalroute@gmail.com>
2026-02-09 11:34:26 +0900

    세영 - 치팅 버튼 수정

--- Assets/BRASS/LSY/Scripts/Reward/BossRewardCheater.cs
+++ Assets/BRASS/LSY/Scripts/Reward/BossRewardCheater.cs
@@ -5,13 +5,19 @@ namespace Team1
 {
     public class BossRewardCheater : MonoBehaviour
     {
-        [SerializeField] private BossClearUI bossClearUI;
+        [SerializeField] private RobotBoss robotBoss; // 보스 스크립트 참조
+        [SerializeField] private BossClearUI bossClearUI; // 보상 UI 참조
 
         void Update()
         {
-            // 신형 방식만 사용해서 에러 해결
-            if (Keyboard.current != null && Keyboard.current.rightBracketKey.wasPressedThisFrame)
+            if (Keyboard.current != null && Keyboard.current.minusKey.wasPressedThisFrame)
             {
+                Debug.Log("- 키 눌림: 치트 발동");
+
+                // 1. 보스 스크립트의 사망 함수 실행 (동작 중지 등)
+                if (robotBoss != null) robotBoss.InstantKill();
+
+                // 2. 보상 UI의 ShowReward 실행 (여기서 마켓/지도를 꺼야 함)
                 if (bossClearUI != null)
                 {
                     bossClearUI.ShowReward();
